3a52620f787825a16c577af806df4443d4d13d3a
方正 <215566435@qq.com>
2018-02-08 23:20:46 +1300

    Rluy的奇怪bug

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import Rluy from 'rluy';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+
+
 Rluy.init();
-Rluy.router(require('./router'));
+// Rluy.router(require('./router'));
+Rluy.JsxElement = require('./router').default();
 Rluy.run(document.getElementById('root'));
 
 registerServiceWorker();

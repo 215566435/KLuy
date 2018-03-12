22172dcf2be37df7f96e341d187b94ad36583266
Z F <215566435@qq.com>
2018-03-12 17:56:23 +1300

    增加一个addfield组件

--- src/views/Editor/log-excersise.js
+++ src/views/Editor/log-excersise.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux'
 import LogForm from './log-form'
 import LogHistory from './log-history'
 import { LoadingArray } from '../../component/LoadingHoc'
+import { AddFields } from './addfield'
 
 const menu = (
     <Menu>
@@ -63,97 +64,6 @@ export const Clickble = ({ children, onClick }) => {
     )
 }
 
-class AddFields extends React.Component {
-    state = {
-        excersiseDetail: []
-    }
-
-    componentDidMount() {
-        // To disabled submit button at the beginning.
-        this.props.form.validateFields()
-    }
-    handleAdd = e => {
-        e.preventDefault()
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values)
-                this.setState({
-                    excersiseDetail: [...this.state.excersiseDetail, values]
-                })
-            }
-        })
-    }
-
-    render() {
-        const {
-            getFieldDecorator,
-            getFieldsError,
-            getFieldError,
-            isFieldTouched
-        } = this.props.form
-
-        // Only show error after a field is touched.
-        const userNameError = isFieldTouched('reps') && getFieldError('reps')
-        const passwordError =
-            isFieldTouched('weight') && getFieldError('weight')
-        return (
-            <div>
-                <Form layout="inline" onSubmit={this.handleSubmit}>
-                    <FormItem
-                        validateStatus={userNameError ? 'error' : ''}
-                        help={userNameError || ''}
-                    >
-                        {getFieldDecorator('reps', {
-                            rules: [
-                                { required: true, message: '请输入次数(数字)' }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon
-                                        type="tag-o"
-                                        style={{ color: 'rgba(0,0,0,.25)' }}
-                                    />
-                                }
-                                placeholder="次数"
-                            />
-                        )}
-                    </FormItem>
-                    <FormItem
-                        validateStatus={passwordError ? 'error' : ''}
-                        help={passwordError || ''}
-                    >
-                        {getFieldDecorator('weight', {
-                            rules: [
-                                { required: true, message: '请输入重量(数字)' }
-                            ]
-                        })(
-                            <Input
-                                prefix={
-                                    <Icon
-                                        type="tag-o"
-                                        style={{ color: 'rgba(0,0,0,.25)' }}
-                                    />
-                                }
-                                placeholder="重量kg"
-                            />
-                        )}
-                    </FormItem>
-                    <FormItem>
-                        <Button
-                            type="primary"
-                            shape="circle"
-                            icon="plus"
-                            onClick={this.handleAdd}
-                            disabled={hasErrors(getFieldsError())}
-                        />
-                    </FormItem>
-                </Form>
-            </div>
-        )
-    }
-}
-
 class Exercise extends React.Component {
     state = {
         visible: false,
@@ -183,11 +93,12 @@ class Exercise extends React.Component {
             visible: false
         })
     }
+    onSubmit = values => {}
 
     render() {
         return (
             <div>
-                <Button type="primary" shape="circle" icon="plus" />
+                <AddFields onSubmit={this.onSubmit} />
                 <LoadingArray array={this.props.exercise}>
                     {item => {
                         return (
